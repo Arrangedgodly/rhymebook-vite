54b6af57b84954951dfeb664745cec29edf81f41
Arrangedgodly <arrangedgodly@gmail.com>
2023-06-01 14:19:14 -0600

    refactor: more landing page animation work

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,7 +5,6 @@ const Landing = () => {
   useEffect(() => {
     new TxtAnime(".typing", {
       effect: "txt-an-7",
-      delayStart: 0,
       delay: 0.05,
       duration: 0.07,
       repeat: true,
@@ -16,9 +15,9 @@ const Landing = () => {
   useEffect(() => {
     new TxtAnime(".rhymes", {
       effect: "txt-an-4",
-      delayStart: 0,
-      delay: 2,
-      duration: 1.00,
+      delayStart: 4,
+      delay: 0.5,
+      duration: 1.5,
       repeat: true,
       text: [
         "exciting lighting fighting biting whiting sighting inciting handwriting underwriting citing",
@@ -28,42 +27,115 @@ const Landing = () => {
     });
   }, []);
 
+  useEffect(() => {
+    new TxtAnime(".soundalikes", {
+      effect: "txt-an-4",
+      delayStart: 4,
+      delay: 0.5,
+      duration: 1.5,
+      repeat: true,
+      text: [
+        "science lightning trying titan abiding striking elighten timing lining whining",
+        "ongoing outgoing folling foregoing easygoing doing smoking rowan hoping growing",
+        "people reason season excited genus treason committed appreciated jesus venus",
+      ],
+    });
+  }, []);
+
+  useEffect(() => {
+    new TxtAnime(".synonyms", {
+      effect: "txt-an-4",
+      delayStart: 4,
+      delay: 0.5,
+      duration: 1.5,
+      repeat: true,
+      text: [
+        "composition authorship penning",
+        "release loss exit departure passing expiration sledding leaving",
+        "triumphant victorious unbowed unbeaten unconquered unvanquished",
+      ],
+    });
+  }, []);
+
+  useEffect(() => {
+    new TxtAnime(".related", {
+      effect: "txt-an-4",
+      delayStart: 4,
+      delay: 0.5,
+      duration: 1.5,
+      repeat: true,
+      text: [
+        "verbal math directing exam poetry screenplay script essays editing sat",
+        "happen nba really",
+        "",
+      ],
+    });
+  }, []);
+
+  useEffect(() => {
+    new TxtAnime(".adjectives", {
+      effect: "txt-an-4",
+      delayStart: 4,
+      delay: 0.5,
+      duration: 1.5,
+      repeat: true,
+      text: [
+        "table desk campaign paper room master process case campaigns pad skills",
+        "concern back further chronicle deeper out time down away places",
+        "season team champion army record seasons teams enemy champions streak",
+      ],
+    });
+  }, []);
+
+  useEffect(() => {
+    new TxtAnime(".nouns", {
+      effect: "txt-an-4",
+      delayStart: 4,
+      delay: 0.5,
+      duration: 1.5,
+      repeat: true,
+      text: [
+        "creative own good historical much automatic academic fine hand american",
+        "church easy cinema sea thorough keep college school get movie",
+        "",
+      ],
+    });
+  }, []);
+
   return (
-    <div className="flex flex-col flex-grow items-center justify-center">
-      <h1 className="text-5xl text-center text-primary-content m-4">
-        Welcome to RhymePage!
-      </h1>
-      <h2 className="text-3xl text-center text-primary-content m-2">
+    <div className="flex flex-col bg-neutral text-neutral-content w-screen flex-grow items-center justify-center">
+      <h1 className="text-5xl text-center m-4">Welcome to RhymePage!</h1>
+      <h2 className="text-3xl text-center m-2">
         An API powered lyric writing application that writes your rhymes with
         you.
       </h2>
       <div className="card bg-secondary text-secondary-content m-4">
         <p className="text-2xl text-center text-secondary-content m-2 typing"></p>
       </div>
-      <div className="card bg-accent text-accent-content flex flex-row items-center m-4">
+      <div className="flex flex-row items-center m-4">
         <div className="flex flex-col items-center m-4 gap-4">
           <h3 className="text-lg">Rhymes</h3>
-          <p className="text-sm text-center text-accent-content rhymes"></p>
+          <p className="text-sm text-center text-accent rhymes"></p>
         </div>
         <div className="flex flex-col items-center justify-center m-4 gap-4">
           <h3 className="text-lg">Sound-Alikes</h3>
-          <p className="text-sm text-center text-accent-content soundalikes"></p>
+          <p className="text-sm text-center text-accent soundalikes"></p>
         </div>
         <div className="flex flex-col items-center justify-center m-4 gap-4">
           <h3 className="text-lg">Synonyms</h3>
-          <p className="text-sm text-center text-accent-content synonyms"></p>
+          <p className="text-sm text-center text-accent synonyms"></p>
         </div>
         <div className="flex flex-col items-center justify-center m-4 gap-4">
-          <h3 className="text-lg">Antonyms</h3>
-          <p className="text-sm text-center text-accent-content antonyms"></p>
+          <h3 className="text-lg">Related Words</h3>
+          <p className="text-sm text-center text-accent related"></p>
         </div>
         <div className="flex flex-col items-center justify-center m-4 gap-4">
           <h3 className="text-lg">Adjectives</h3>
-          <p className="text-sm text-center text-accent-content adjectives"></p>
+          <p className="text-sm text-center text-accent adjectives"></p>
         </div>
         <div className="flex flex-col items-center justify-center m-4 gap-4">
-          <h3 className="text-lg">Frequent Followers</h3>
-          <p className="text-sm text-center text-accent-content freqfollowers"></p>
+          <h3 className="text-lg">Nouns</h3>
+          <p className="text-sm text-center text-accent nouns"></p>
         </div>
       </div>
     </div>
